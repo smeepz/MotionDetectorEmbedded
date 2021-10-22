b054bac0ca9feb6209003975e496760d9e68cb7e
smeepz <stianlw@gmail.com>
2021-10-22 19:49:07 +0200

    finnaly worked out the bug with the TFT-screen. It now prints the text the right way, and the strcat is used for concating the char arrays with tmp-buffers.

--- Arbeidskrav_2_V1.ino
+++ Arbeidskrav_2_V1.ino
@@ -14,20 +14,25 @@ long cm;
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
 #include <SPI.h>
-#include <Fonts/FreeMonoBoldOblique24pt7b.h>
+#include <Fonts/FreeMonoBoldOblique18pt7b.h>
 
 #if defined(ARDUINO_FEATHER_ESP32) // Feather Huzzah32
-  #define TFT_CS         14
-  #define TFT_RST        15
-  #define TFT_DC         32
+#define TFT_CS         14
+#define TFT_RST        15
+#define TFT_DC         32
 
 #else
-  // For the breakout board, you can use any 2 or 3 pins.
-  // These pins will also work for the 1.8" TFT shield.
-  #define TFT_CS        10
-  #define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
-  #define TFT_DC         8
+// For the breakout board, you can use any 2 or 3 pins.
+// These pins will also work for the 1.8" TFT shield.
+#define TFT_CS        10
+#define TFT_RST        9 // Or set to -1 and connect to Arduino RESET pin
+#define TFT_DC         8
 #endif
+
+#include <Wire.h>
+#include <RtcDS3231.h>
+RtcDS3231<TwoWire> Rtc(Wire);
+#define countof(a) (sizeof(a) / sizeof(a[0]))
 
 // For 1.14", 1.3", 1.54", 1.69", and 2.0" TFT with ST7789:
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
@@ -37,7 +42,7 @@ void setup() {
 
   Serial.begin(9600);
 
-// OR use this initializer (uncomment) if using a 1.14" 240x135 TFT:
+  // OR use this initializer (uncomment) if using a 1.14" 240x135 TFT:
   tft.init(135, 240);           // Init ST7789 240x135
 
   pinMode(transmitterPin, OUTPUT);
@@ -50,32 +55,37 @@ void setup() {
 
   pinMode(buzzerPin, OUTPUT);
 
+  Rtc.Begin();
+  RtcDateTime now = Rtc.GetDateTime();
+  Rtc.Enable32kHzPin(false);
+  Rtc.SetSquareWavePin(DS3231SquareWavePin_ModeNone);
+
 }
 
 void loop() {
 
-
   rangeIndicator();
+  delay(2000);
 }
 
-void testdrawtext(char *text, uint16_t color) {
+void drawText(char *text, uint16_t color) {
   tft.setRotation(1);
-  tft.setFont(&FreeMonoBoldOblique24pt7b);
+  tft.setFont(&FreeMonoBoldOblique18pt7b);
   tft.fillScreen(ST77XX_BLACK);
-  tft.setCursor(0,75);
+  tft.setCursor(0, 25);
   tft.setTextColor(color);
   tft.setTextWrap(true);
   tft.print(text);
 }
 
 void ledLightColor(int redValue, int greenValue, int blueValue) {
-    analogWrite(redLedPin, redValue);
-    analogWrite(greenLedPin, greenValue);
-    analogWrite(blueLedPin, blueValue);
+  analogWrite(redLedPin, redValue);
+  analogWrite(greenLedPin, greenValue);
+  analogWrite(blueLedPin, blueValue);
 }
 
 void rangeIndicator() {
-
+  RtcDateTime now = Rtc.GetDateTime();
   digitalWrite(transmitterPin, LOW);
   delayMicroseconds(5);
   digitalWrite(transmitterPin, HIGH);
@@ -85,22 +95,41 @@ void rangeIndicator() {
   pinMode(recieverPin, INPUT);
   duration = pulseIn(recieverPin, HIGH);
 
-  cm = (duration / 2) / 29.1; //The calculation for centimeters
+  char timeStamp[6];
 
+  cm = (duration / 2) / 29.1; //The calculation for centimeters
   if (cm >= 150) {
-    Serial.println("--------OK");
     ledLightColor(0, 255, 0);
-    testdrawtext("OK", ST77XX_GREEN);
+    char bufOk[10];
+    char msgOk[3] = "OK";
+    timeStamp[6] = printDateTime(now);
+    strcat(bufOk, timeStamp);
+    strcat(bufOk, " ");
+    strcat(bufOk, msgOk);
+    drawText(bufOk, ST77XX_GREEN);
     digitalWrite(buzzerPin, LOW);
   } else {
-    Serial.println("--------!WARNING!");
     ledLightColor(255, 0, 0);
-    testdrawtext("INTRUDER", ST77XX_RED);
+    char bufAlert[13];
+    char msgAlert[6] = "ALERT";
+    timeStamp[6] = printDateTime(now);
+    strcat(bufAlert, timeStamp);
+    strcat(bufAlert, " ");
+    strcat(bufAlert, msgAlert);
+    drawText(bufAlert, ST77XX_RED);
     digitalWrite(buzzerPin, HIGH);
   }
-
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println(); //Format prints
   delay(750);
+}
+
+char printDateTime(const RtcDateTime& dt)
+{
+  char datestring[6];
+  snprintf_P(datestring,
+             countof(datestring),
+             PSTR("%02u:%02u"),
+             dt.Hour(),
+             dt.Minute()
+            );
+  return datestring;
 }
